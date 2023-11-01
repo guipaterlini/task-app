85be4025b104287b0a6527c30aabbc918929e621
Guilherme Paterlini <guilhermepaterlini@gmail.com>
2023-10-31 22:56:48 -0400

    Seting up useState and rendering Task List

--- src/components/TaskList.js
+++ src/components/TaskList.js
@@ -1,7 +1,23 @@
+import { useState } from "react";
+
 export default function TaskList() {
+  const [tasks, setTasks] = useState([
+    "Tarefa 1",
+    "Tarefa 2",
+    "Tarefa 3",
+    "Tarefa 4",
+    "Tarefa 5",
+    "Tarefa 6",
+  ]);
+
   return (
     <div>
-      <h1>Hello World</h1>
+      <h1>Task to do:</h1>
+      <ul>
+        {tasks.map((task, index) => (
+          <li key={index}>{task} </li>
+        ))}
+      </ul>
     </div>
   );
 }
