2cab957b9527896614a271f0ae622f37211cb468
Guilherme Paterlini <guilhermepaterlini@gmail.com>
2024-09-30 14:04:53 -0400

    adiciona botão para crir nova task

--- src/components/TaskBoard.jsx
+++ src/components/TaskBoard.jsx
@@ -4,7 +4,11 @@ import styles from "./TaskBoard.module.css";
 export function TaskBoard() {
   return (
     <div className={styles.taskBoard}>
-      <h1>To Do List</h1>
+      <header>
+        <h1>To Do List</h1>
+        <button>+ new</button>
+      </header>
+
       <TaskItem
         title="Tarefa 1"
         description="Lorem ipsum dolor, sit amet consectetur adipisicing elit. Cumque, non!"
