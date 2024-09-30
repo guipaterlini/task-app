3244aaa437cb8d11fb859a214cdb3994ddc9ccee
Guilherme Paterlini <guilhermepaterlini@gmail.com>
2024-09-30 11:44:10 -0400

    estiliza o fundo e taskboard

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,20 @@
 import { TaskItem } from "./components/TaskItem";
+import "./styles.css";
 
 export function App() {
   return (
     <div>
-      <h1>To Do List</h1>
-      <TaskItem
-        title="Tarefa 1"
-        description="Lorem ipsum dolor, sit amet consectetur adipisicing elit. Cumque, non!"
-      />
-      <TaskItem
-        title="Tarefa 2"
-        description="Lorem ipsum dolor, sit amet consectetur adipisicing elit. Cumque, non!"
-      />
+      <main className="taskBoard">
+        <h1>To Do List</h1>
+        <TaskItem
+          title="Tarefa 1"
+          description="Lorem ipsum dolor, sit amet consectetur adipisicing elit. Cumque, non!"
+        />
+        <TaskItem
+          title="Tarefa 2"
+          description="Lorem ipsum dolor, sit amet consectetur adipisicing elit. Cumque, non!"
+        />
+      </main>
     </div>
   );
 }
