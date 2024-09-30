400d484d159a867a297f9f788e28e63e8957af7d
Guilherme Paterlini <guilhermepaterlini@gmail.com>
2024-09-30 14:02:18 -0400

    Organize melhor os componentes e estiliza usando css modules

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,10 @@
-import { TaskItem } from "./components/TaskItem";
+import { TaskBoard } from "./components/TaskBoard";
 import "./global.css";
 
 export function App() {
   return (
     <div>
-      <main className="taskBoard">
-        <h1>To Do List</h1>
-        <TaskItem
-          title="Tarefa 1"
-          description="Lorem ipsum dolor, sit amet consectetur adipisicing elit. Cumque, non!"
-        />
-        <TaskItem
-          title="Tarefa 2"
-          description="Lorem ipsum dolor, sit amet consectetur adipisicing elit. Cumque, non!"
-        />
-      </main>
+      <TaskBoard />
     </div>
   );
 }
---
+++ src/components/TaskBoard.jsx
@@ -0,0 +1,18 @@
+import { TaskItem } from "./TaskItem";
+import styles from "./TaskBoard.module.css";
+
+export function TaskBoard() {
+  return (
+    <div className={styles.taskBoard}>
+      <h1>To Do List</h1>
+      <TaskItem
+        title="Tarefa 1"
+        description="Lorem ipsum dolor, sit amet consectetur adipisicing elit. Cumque, non!"
+      />
+      <TaskItem
+        title="Tarefa 2"
+        description="Lorem ipsum dolor, sit amet consectetur adipisicing elit. Cumque, non!"
+      />
+    </div>
+  );
+}
